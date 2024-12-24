3c5782ea67c94fb551c5b61d462c584929b3b87e
Wanxuan Li <lzlu1018@gmail.com>
2024-12-24 12:38:15 -0800

    yea

--- archive/1b5dozl.cpp
+++ archive/1b5dozl.cpp
@@ -4,17 +4,33 @@ using namespace std;
 int n;
 string s;
 
-signed main(){
-    int t; cin>>t;
-    while(t--){
-        cin>>n>>s;
-        int cnt=0;
+void solve(){
+    cin>>n>>s;
+    int cnt=0;
+    for(int i=0; i<n; ++i)
+        if(s[i]=='1')
+            cnt++;
+    if(cnt%2){
+        cout<<"NO\n";
+        return;
+    }
+
+    // the only dangerous case is cnt=2 and two adjcent 1s
+    if(cnt==2){
+        vector<int> pos;
         for(int i=0; i<n; ++i)
             if(s[i]=='1')
-                cnt++;
-        if(cnt%2)
+                pos.push_back(i);
+        if(pos[0]+1 == pos[1]){
             cout<<"NO\n";
-        else
-            cout<<"YES\n";
+            return;
+        }
     }
+
+    cout<<"YES\n";
+}
+
+signed main(){
+    int t; cin>>t;
+    while(t--) solve();
 }
