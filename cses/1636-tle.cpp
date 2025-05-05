4f2cec752ea93e0fecebd2c5e7e9e79c00505b74
Wanxuan Li <lzlu1018@gmail.com>
2025-05-05 14:01:56 -0400

    recursive sol tle

---
+++ cses/1636-tle.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int mod = 1e9 + 7;
+
+// to get the distinct number of ways, that means we need to maintain
+// non-decreasing order in the indices
+
+int bktk(int i, int sum, vector<int>& a, vector<vector<int>>& dp) {
+    if (sum < 0) {
+        return 0;
+    }
+    if (sum == 0) {
+        return 1;
+    }
+    if (dp[i][sum] != -1) {
+        return dp[i][sum];
+    }
+    int ans = 0;
+    for (int j = i; j < a.size(); ++j) {
+        // note the difference: sum - a[j] and sum - a[i];
+        // the dp state should be defined as:
+        // dp[i][j] contains the distinct ways to construct j with the last used index i
+        ans = (ans + bktk(j, sum - a[j], a, dp)) % mod;
+    }
+    return dp[i][sum] = ans;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    vector<vector<int>> dp(n+1, vector<int>(x+1, -1));
+    int ans = bktk(0, x, a, dp);
+    cout << ans << '\n';
+}
