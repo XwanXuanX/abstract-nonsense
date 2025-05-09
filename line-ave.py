0bdf8258c3bd77cfabf043ca1c5631ae43e05413
Wanxuan Li <lzlu1018@gmail.com>
2025-05-09 00:05:27 -0400

    full upgrade

--- line-ave.py
+++ line-ave.py
@@ -1,44 +1,84 @@
-# Print the average number of lines of code that I've ever written
-# Goal: THE FEWER THE BETTER!
-
 import os
 import csv
 from datetime import date
 from pathlib import Path
 from contextlib import closing
 
-count: int = 0
-total: int = 0
 
-for filename in os.listdir(r"./cf/"):
-    # only count cpp files
-    if Path(filename).suffix != ".cpp":
-        continue
+###################################################
+# Add more folders if necessary
+problem_folders: list[str] = [
+    r"./cf/",
+    r"./cses/",
+    r"./random/",
+]
+###################################################
 
-    count += 1
 
-    with closing(open(os.path.join(r"./cf", filename))) as file:
-        lines = file.readlines()
-        total += len(lines)
+def get_stats(problem_folders: list[str]) -> tuple[int, int]:
+    """
+    Iterate over the specified problem folders and produce the
+    number of files and number of lines in total
+    """
 
-# output the result
-result = total / count
-print(f"Average number lines of code: {result}")
+    files: int = 0
+    lines: int = 0
 
-# log the result to a csv file
-# we can check if there's any update to cf since last logging, if non, ignore this action
-TOTAL_FILE = 1
-LOG_FILE: str = "codelens.csv"
+    for folder in problem_folders:
+        for filename in os.listdir(folder):
+            # Only count cpp files
+            if Path(filename).suffix != ".cpp":
+                continue
 
-with closing(open(LOG_FILE, 'r')) as csvfile:
-    reader = csv.reader(csvfile)
-    lines = [line for line in reader]
-    last = lines[len(lines) - 1]
-    if int(last[TOTAL_FILE]) == count:
-        print("No new files added. Action ignored.")
-        exit(1)
+            files += 1
+            with closing(open(os.path.join(folder, filename))) as file:
+                file_lines = file.readlines()
+                lines += len(file_lines)
 
-# otherwise, we can proceed to log the result
-with open(LOG_FILE, 'a', newline='\n') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow([date.today(), count, total, result])
+    return (files, lines)
+
+
+def output_stats(stats: tuple[int, int], to_file: bool = True) -> None:
+    """
+    Output stats to stdout and (optionally) to csv file
+    """
+
+    files, lines = stats
+    average = lines / files
+
+    print(
+        f"Files ever created: {files}\n"
+        f"Lines ever wrote: {lines}\n"
+        f"Average lines per file: {average}"
+    )
+
+    if not to_file:
+        return
+    
+    # Log the result to a csv file
+    # We can check if there's any update to cf since last logging
+    # If non, ignore this action
+    TOTAL_FILE = 1
+    LOG_FILE: str = "codelens.csv"
+    with closing(open(LOG_FILE, 'r')) as csvfile:
+        reader = csv.reader(csvfile)
+        file_lines = [line for line in reader]
+        last = file_lines[len(file_lines) - 1]
+        if int(last[TOTAL_FILE]) == files:
+            print("No new files added. Not writing to file.")
+            exit(1)
+
+    with open(LOG_FILE, 'a', newline='\n') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([date.today(), files, lines, average])
+    
+    return
+
+
+def main():
+    output_stats(get_stats(problem_folders))
+    return
+
+
+if __name__ == "__main__":
+    main()
