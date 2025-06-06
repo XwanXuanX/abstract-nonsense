0615b57f7f665a521b8e0b5f56745af8a7a77be0
Wanxuan Li <lzlu1018@gmail.com>
2025-06-06 19:07:22 -0400

    minor update

--- template.cpp
+++ template.cpp
@@ -2,21 +2,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define all(x)                      x.begin(), x.end()
-#define rep(x)                      for (int _ = (0); _ < (x); ++_)
-template <typename T> using V       = vector<T>;
-template <typename T> using pr      = pair<T, T>;
-template <typename T> using minpq   = priority_queue<T, V<T>, greater<T>>;
-template <typename T> using maxpq   = priority_queue<T, V<T>, less<T>>;
+#define all(x)                          x.begin(), x.end()
+#define rep(x)                          for (int _ = (0); _ < (x); ++_)
+template <typename T> using V           = vector<T>;
+template <typename T> using pr          = pair<T, T>;
+template <typename T> using minpq       = priority_queue<T, V<T>, greater<T>>;
+template <typename T> using maxpq       = priority_queue<T, V<T>, less<T>>;
 
-template <typename, typename = void> struct DefaultI : std::false_type {};
 template <typename T> struct DefaultI<T, std::void_t<decltype(std::cin  >> std::declval<T&>())>> : std::true_type {};
-template <typename, typename = void> struct DefaultO : std::false_type {};
 template <typename T> struct DefaultO<T, std::void_t<decltype(std::cout << std::declval<T&>())>> : std::true_type {};
-template <typename, typename = void> struct IsTuples : std::false_type {};
 template <typename T> struct IsTuples<T, std::void_t<typename std::tuple_size<T>::type>>         : std::true_type {};
-template <typename, typename = void> struct Iterable : std::false_type {};
 template <typename T> struct Iterable<T, std::void_t<decltype(std::begin(std::declval<T>()))>>   : std::true_type {};
+template <typename, typename = void> struct DefaultI : std::false_type {};
+template <typename, typename = void> struct DefaultO : std::false_type {};
+template <typename, typename = void> struct IsTuples : std::false_type {};
+template <typename, typename = void> struct Iterable : std::false_type {};
 template <typename T> char sp(T& t) {return(Iterable<T>::value||IsTuples<T>::value)?'\n':' ';};
 template <typename T> void re(T& t) {if constexpr(DefaultI<T>::value){cin>>t;}else if constexpr(Iterable<T>::value){for(auto&x:t)re(x);}else if constexpr(IsTuples<T>::value){std::apply([&](auto&...args){(re(args),...);},t);}};
 template <typename T> void pt(T& t) {if constexpr(DefaultO<T>::value){cout<<t;}else if constexpr(Iterable<T>::value){int i=0;for(auto&&x:t)((i++)?(cout<<sp(x),pt(x)):pt(x));}else if constexpr(IsTuples<T>::value){std::apply([&](auto const&...args){int i=0;(((i++)?(cout<<" ",pt(args)):pt(args)),...);},t);}};
@@ -41,10 +41,6 @@ signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     int tt = 1;
-#ifndef single
-    re(tt);
-#endif
-    while (tt--) {
-        solve();
-    }
+    read(tt);
+    rep(tt) solve();
 }
