d209f2adc997ae8a1aaf835aee67363f29765fa6
Wanxuan Li <lzlu1018@gmail.com>
2025-06-06 18:57:18 -0400

    just do better

--- template.cpp
+++ template.cpp
@@ -1,43 +1,45 @@
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
-#pragma GCC optimize("O3")
 using namespace std;
-/*********************************************************************************************************************************************************/
-#define SFINAE(x, ...) template <class, class = void> struct x : std::false_type {}; template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
-SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>())); SFINAE(DefaultO, decltype(std::cout << std::declval<T &>())); SFINAE(IsTuple, typename std::tuple_size<T>::type); SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
-template <auto &is> struct Reader {
-template <class T> void Impl(T &t) { if constexpr (DefaultI<T>::value) { is >> t; } else if constexpr (Iterable<T>::value) { for (auto &x : t) Impl(x); } else if constexpr (IsTuple<T>::value) { std::apply([this](auto &...args) { (Impl(args), ...); }, t); } else { static_assert(IsTuple<T>::value, "No matching type for read"); } }
-template <class... Ts> void read(Ts &...ts) { ((Impl(ts)), ...); } };
-template <auto &os, bool print_nd> struct Writer {
-template <class T> constexpr char Space(const T &) const { return print_nd && (Iterable<T>::value or IsTuple<T>::value) ? '\n' : ' '; }
-template <class T> void Impl(T const &t) const { if constexpr (DefaultO<T>::value) { os << t; } else if constexpr (Iterable<T>::value) { int i = 0; for (auto &&x : t) { ((i++) ? (os << Space(x), Impl(x)) : Impl(x)); } } else if constexpr (IsTuple<T>::value) { std::apply([this](auto const &...args) {int i = 0; (((i++) ? (os << " ", Impl(args)) : Impl(args)), ...);}, t); } else { static_assert(IsTuple<T>::value, "No matching type for print"); } }
-template <class F, class... Ts> void print_with_sep(const std::string &sep, F const &f, Ts const &...ts) const { Impl(f), ((os << sep, Impl(ts)), ...), os << '\n'; }
-template <class... Ts> void print(Ts const &...ts) const { ((Impl(ts)), ...); }
-void print_with_sep(const std::string &) const { os << '\n'; } };
-/******************************************************************* IO **********************************************************************************/
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0), cout.tie(0)
-template <class... Ts> void re(Ts &...ts) { Reader<cin>{}.read(ts...); }
-template <class... Ts> void pr(Ts const &...ts) { Writer<cout, true>{}.print(ts...); }
-template <class... Ts> void ps(Ts const &...ts) { Writer<cout, true>{}.print_with_sep(" ", ts...); }
-/****************************************************************** TOOL *********************************************************************************/
-template<typename T> using V = vector<T>; template<typename T, size_t SZ> using AR = array<T, SZ>;
-template<typename T> using pqg = priority_queue<T, V<T>, greater<T>>; // return smallest
-template<typename T> using pql = priority_queue<T, V<T>, less<T>>; // return largest
-constexpr int popcount(int x) { return __builtin_popcount(x); }  // # of bits set
-constexpr int floorlog2(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }  // floor(log2(x))
-constexpr long long ceildiv(long long a, long long b) { return a / b + ((a ^ b) > 0 && a % b); }  // divide a by b rounded up
-constexpr long long floordiv(long long a, long long b) { return a / b - ((a ^ b) < 0 && a % b); }  // divide a by b rounded down
-/***************************************************************** CONSTS ********************************************************************************/
-constexpr int MOD = 1e9 + 7;
-constexpr long long INF = 1e18;  // not too close to LLONG_MAX
-const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  // for every grid problem!!
-/*********************************************************************************************************************************************************/
+
+#define all(x)                      x.begin(), x.end()
+#define rep(x)                      for (int _ = (0); _ < (x); ++_)
+template <typename T> using V       = vector<T>;
+template <typename T> using pr      = pair<T, T>;
+template <typename T> using minpq   = priority_queue<T, V<T>, greater<T>>;
+template <typename T> using maxpq   = priority_queue<T, V<T>, less<T>>;
+
+template <typename, typename = void> struct DefaultI : std::false_type {};
+template <typename T> struct DefaultI<T, std::void_t<decltype(std::cin  >> std::declval<T&>())>> : std::true_type {};
+template <typename, typename = void> struct DefaultO : std::false_type {};
+template <typename T> struct DefaultO<T, std::void_t<decltype(std::cout << std::declval<T&>())>> : std::true_type {};
+template <typename, typename = void> struct IsTuples : std::false_type {};
+template <typename T> struct IsTuples<T, std::void_t<typename std::tuple_size<T>::type>>         : std::true_type {};
+template <typename, typename = void> struct Iterable : std::false_type {};
+template <typename T> struct Iterable<T, std::void_t<decltype(std::begin(std::declval<T>()))>>   : std::true_type {};
+template <typename T> char sp(T& t) {return(Iterable<T>::value||IsTuples<T>::value)?'\n':' ';};
+template <typename T> void re(T& t) {if constexpr(DefaultI<T>::value){cin>>t;}else if constexpr(Iterable<T>::value){for(auto&x:t)re(x);}else if constexpr(IsTuples<T>::value){std::apply([&](auto&...args){(re(args),...);},t);}};
+template <typename T> void pt(T& t) {if constexpr(DefaultO<T>::value){cout<<t;}else if constexpr(Iterable<T>::value){int i=0;for(auto&&x:t)((i++)?(cout<<sp(x),pt(x)):pt(x));}else if constexpr(IsTuples<T>::value){std::apply([&](auto const&...args){int i=0;(((i++)?(cout<<" ",pt(args)):pt(args)),...);},t);}};
+template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) {pt(t),((cout<<s,pt(ts)),...),cout<<'\n';}; void ps(const string&) {cout<<'\n';};
+template <typename... Ts> void read(Ts& ...ts) {((re(ts)), ...);};
+template <typename... Ts> void print(Ts& ...ts) {ps(" ", ts...);};
+
+template <typename T> int  lwb(const V<T>& a, const T& b) {return int(lower_bound(all(a),b) - a.begin());};
+template <typename T> int  upb(const V<T>& a, const T& b) {return int(upper_bound(all(a),b) - a.begin());};
+template <typename T> void remDup(V<T>& v) {sort(all(v));v.erase(unique(all(v)), v.end());};
+template <typename T> T    cdiv(T a, T b) requires std::integral<T> {return a/b + ((a^b) > 0 && a%b);};
+
+// Open in case of emergency X_X:
+// #define int      long long
+// #define double   long double
 
 void solve() {
     
 }
 
 signed main() {
-    fastIO;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
     int tt = 1;
 #ifndef single
     re(tt);
