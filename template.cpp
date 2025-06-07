efcdfcaa7b8c23abefa2be8ae963a860b7f3ed3a
Wanxuan Li <lzlu1018@gmail.com>
2025-06-07 18:35:01 -0400

    print accept rv

--- template.cpp
+++ template.cpp
@@ -17,13 +17,13 @@ template <typename T> char sp(T& t) {return(Iterable<T>::value||IsTuples<T>::val
 template <typename T> void re(T& t) {if constexpr(DefaultI<T>::value){cin>>t;}else if constexpr(Iterable<T>::value){for(auto&x:t)re(x);}else if constexpr(IsTuples<T>::value){std::apply([&](auto&...args){(re(args),...);},t);}};
 template <typename T> void pt(T& t) {if constexpr(DefaultO<T>::value){cout<<t;}else if constexpr(Iterable<T>::value){int i=0;for(auto&&x:t)((i++)?(cout<<sp(x),pt(x)):pt(x));}else if constexpr(IsTuples<T>::value){std::apply([&](auto const&...args){int i=0;(((i++)?(cout<<" ",pt(args)):pt(args)),...);},t);}};
 template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) {pt(t),((cout<<s,pt(ts)),...),cout<<'\n';}; void ps(const string&) {cout<<'\n';};
+template <typename... Ts> void print(const Ts& ...ts) {ps(" ", ts...);};
 template <typename... Ts> void read(Ts& ...ts) {((re(ts)), ...);};
-template <typename... Ts> void print(Ts& ...ts) {ps(" ", ts...);};
 
 template <typename T> int  lwb(const V<T>& a, const T& b) {return int(lower_bound(all(a),b) - a.begin());};
 template <typename T> int  upb(const V<T>& a, const T& b) {return int(upper_bound(all(a),b) - a.begin());};
-template <typename T> void remDup(V<T>& v) {sort(all(v));v.erase(unique(all(v)), v.end());};
 template <typename T> T    cdiv(T a, T b) requires std::integral<T> {return a/b + ((a^b) > 0 && a%b);};
+template <typename T> void remDup(V<T>& v) {sort(all(v));v.erase(unique(all(v)), v.end());};
 
 // Open in case of emergency X_X:
 // #define int      long long
