c83e5dcadf95b05ed26cb7680db66849248ffc67
Wanxuan Li <lzlu1018@gmail.com>
2025-06-12 16:29:01 -0400

    modify addition proofs

--- proofs/Proofs/AdditionWorld.lean
+++ proofs/Proofs/AdditionWorld.lean
@@ -4,33 +4,46 @@ import Mathlib.Data.Nat.Basic
 namespace AdditionWorld
 
 open TutorialWorld
+open MyNat
 
-theorem zero_add (n : ℕ) : 0 + n = n := by
+theorem zero_add (n : MyNat) : 0 + n = n := by
   induction n with
-  | zero => rw [add_zero 0]
-  | succ d hd => rw[← succ_eq_add_one, add_succ, hd]
+    | zero =>
+        rfl
+    | succ d hd =>
+        rw[add_succ, hd]
 
-theorem succ_add (a b : ℕ) : succ a + b = succ (a + b) := by
+theorem succ_add (a b : MyNat) : succ a + b = succ (a + b) := by
   induction b with
-  | zero => rw [← add_zero, ← add_zero]
-  | succ d hd => repeat rw [← succ_eq_add_one]; repeat rw [add_succ]
-                 rw [hd]
+    | zero =>
+        rfl
+    | succ d hd =>
+        repeat rw [add_succ]
+        rw [hd]
 
-theorem add_comm (a b : ℕ) : a + b = b + a := by
+theorem add_comm (a b : MyNat) : a + b = b + a := by
   induction b with
-  | zero => rw [add_zero a, zero_add]
-  | succ d hd => repeat rw [← succ_eq_add_one]; rw [add_succ, succ_add, hd]
+    | zero =>
+        change a + 0 = 0 + a
+        rw [← add_zero, zero_add]
+    | succ d hd =>
+        rw [add_succ, succ_add, hd]
 
-theorem add_assoc (a b c : ℕ) : a + b + c = a + (b + c) := by
+theorem add_assoc (a b c : MyNat) : a + b + c = a + (b + c) := by
   induction a with
-  | zero => repeat rw [zero_add]
-  | succ d hd => repeat rw [← succ_eq_add_one]; repeat rw [succ_add]
-                 rw [hd]
+    | zero =>
+        change 0 + b + c = 0 + (b + c)
+        repeat rw [zero_add]
+    | succ d hd =>
+        repeat rw [succ_add]
+        rw [hd]
 
-theorem add_right_comm (a b c : ℕ) : a + b + c = a + c + b := by
+theorem add_right_comm (a b c : MyNat) : a + b + c = a + c + b := by
   induction b with
-  | zero => rw [← add_zero, ← add_zero]
-  | succ d hd => repeat rw [← succ_eq_add_one]
-                 rw [add_succ, succ_add, add_succ, hd]
+    | zero =>
+        change a + 0 + c = a + c + 0
+        rw [← add_zero, ← add_zero]
+    | succ d hd =>
+        rw [add_succ, succ_add, add_succ, hd]
 
 end AdditionWorld
