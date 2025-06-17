1abdd160ec48f02fa868384a1128c750c1287b2c
Wanxuan Li <lzlu1018@gmail.com>
2025-06-16 23:29:00 -0400

    why isPrefix works

--- proofs/Proofs/Algo/Palindrome.lean
+++ proofs/Proofs/Algo/Palindrome.lean
@@ -2,6 +2,7 @@ import Mathlib.Data.List.Basic
 import Mathlib.Tactic
 
 variable {α : Type}
+variable {a : α}
 variable {as : List α}
 
 inductive Palindrome : List α → Prop where
@@ -74,5 +75,104 @@ Have fun proving :)
 example (as : List α) (h : as = []) : Palindrome as := by {
   simp_all [Palindrome.nil]
 }
+
+-- 2. A singleton list (a list with one element) is a palindrome.
+example (a : α) (as : List α) (h : as = [a]) : Palindrome as := by {
+  simp_all [Palindrome.single]
+}
+
+-- 3. If as is a palindrome, then so is [a] ++ as ++ [a] for any element a.
+example (h : Palindrome as) : Palindrome ([a] ++ as ++ [a]) := by {
+  exact Palindrome.sandwich a as h
+}
+
+-- 4. The reverse of a palindrome is a palindrome.
+example (h : Palindrome as) : Palindrome (as.reverse) := by {
+  induction h with
+  | nil => exact Palindrome.nil;
+  | single a => exact Palindrome.single a;
+  | sandwich a as h ih => {
+    rw [rev_eq];
+    exact Palindrome.sandwich a as.reverse ih;
+  }
+}
+
+-- This is a hard one:
+-- 5. Every prefix of a palindrome that is also a suffix of the same length is itself a palindrome.
+-- To formalize this statement, we need to first formally define what's a prefix and suffix
+
+inductive Prefix : List α → List α → Prop where
+  | nil   : ∀ l, Prefix [] l
+  | cons  : ∀ a l₁ l₂, Prefix l₁ l₂ → Prefix (a :: l₁) (a :: l₂)
+
+inductive Suffix : List α → List α → Prop where
+  | nil   : ∀ l, Suffix l []
+  | cons  : ∀ a l₁ l₂, Suffix l₁ l₂ → Suffix (l₁ ++ [a]) (l₂ ++ [a])
+
+def isPrefix [DecidableEq α] (as ab: List α) : Bool :=
+  match as, ab with
+  | [], _ => true -- empty list is always prefix of any list
+  | _::__, [] => false -- non-empty list cannot be prefix of empty list
+  | a::asc, b::abc => if a = b then isPrefix asc abc else false
+
+-- Proves that the function `isPrefix` is always correct
+theorem is_prefix_correct [DecidableEq α] (as ab : List α) : isPrefix as ab  ↔ Prefix as ab := by {
+  constructor
+  show isPrefix as ab = true → Prefix as ab; {
+    intro h
+    cases as with
+    | nil => exact Prefix.nil ab
+    | cons a as' =>
+      cases ab with
+      | nil => contradiction
+      | cons b ab' =>
+        by_cases hab : a = b
+        · -- Case a = b
+          have : isPrefix as' ab' = true := by
+            rw [isPrefix, hab] at h
+            simp at h
+            exact h
+          -- Now we have the inductive hypothesis
+          rw [← hab]
+          exact Prefix.cons a as' ab' ((is_prefix_correct as' ab').mp this)
+        · -- Case a ≠ b
+          rw [isPrefix, if_neg hab] at h
+          contradiction
+  }
+  show Prefix as ab → isPrefix as ab = true; {
+    intro h
+    induction h with
+    | nil ab => rw [isPrefix]
+    | cons a as' ab' h' ih =>
+      rw [isPrefix, if_pos (by rfl)]
+      exact ih
+  }
+}
+
+
+
+
+
+-- def isSuffix [DecidableEq α] (as : List α) (ab : List α) : Bool :=
+--   match as, ab with
+--   | [], _ => true
+
+
+
+section check
+
+variable {α : Type}
+#eval isPrefix [] [1]
+#eval isPrefix [1] []
+#eval isPrefix [1, 2, 3] [1, 2, 3, 4, 5]
+#eval isPrefix [1, 2, 3] [1, 3, 5, 7, 9]
+
+end check
+
+
+
+
+
+
 
 end PalindromeFacts
