2dc230d5213d4770a21e2132cd819b4b66332fc7
Wanxuan Li <lzlu1018@gmail.com>
2025-08-19 21:01:18 +0800

    adhoc39

---
+++ cf/2111B.cpp
@@ -0,0 +1,78 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x)                          x.begin(), x.end()
+#define rep(x)                          for (int _ = (0); _ < (x); ++_)
+template <typename T> using V           = vector<T>;
+template <typename T> using pr          = pair<T, T>;
+template <typename T> using minpq       = priority_queue<T, V<T>, greater<T>>;
+template <typename T> using maxpq       = priority_queue<T, V<T>, less<T>>;
+
+template <typename T, typename = void> struct DefaultI : std::false_type {}; template <typename T> struct DefaultI<T, std::void_t<decltype(std::cin  >> std::declval<T&>())>> : std::true_type {};
+template <typename T, typename = void> struct DefaultO : std::false_type {}; template <typename T> struct DefaultO<T, std::void_t<decltype(std::cout << std::declval<T&>())>> : std::true_type {};
+template <typename T, typename = void> struct IsTuples : std::false_type {}; template <typename T> struct IsTuples<T, std::void_t<typename std::tuple_size<T>::type>>         : std::true_type {};
+template <typename T, typename = void> struct Iterable : std::false_type {}; template <typename T> struct Iterable<T, std::void_t<decltype(std::begin(std::declval<T>()))>>   : std::true_type {};
+template <typename T> char sp(T& t) {return(Iterable<T>::value||IsTuples<T>::value)?'\n':' ';};
+template <typename T> void re(T& t) {if constexpr(DefaultI<T>::value){cin>>t;}else if constexpr(Iterable<T>::value){for(auto&x:t)re(x);}else if constexpr(IsTuples<T>::value){std::apply([&](auto&...args){(re(args),...);},t);}};
+template <typename T> void pt(T& t) {if constexpr(DefaultO<T>::value){cout<<t;}else if constexpr(Iterable<T>::value){int i=0;for(auto&&x:t)((i++)?(cout<<sp(x),pt(x)):pt(x));}else if constexpr(IsTuples<T>::value){std::apply([&](auto const&...args){int i=0;(((i++)?(cout<<" ",pt(args)):pt(args)),...);},t);}};
+template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) {pt(t),((cout<<s,pt(ts)),...),cout<<'\n';}; void ps(const string&) {cout<<'\n';};
+template <typename... Ts> void print(const Ts& ...ts) {ps(" ", ts...);};
+template <typename... Ts> void read(Ts& ...ts) {((re(ts)), ...);};
+
+template <typename T> int  lwb(const V<T>& a, const T& b) {return int(lower_bound(all(a),b) - a.begin());};
+template <typename T> int  upb(const V<T>& a, const T& b) {return int(upper_bound(all(a),b) - a.begin());};
+template <typename T> T    cdiv(T a, T b) requires std::integral<T> {return a/b + ((a^b) > 0 && a%b);};
+template <typename T> void remDup(V<T>& v) {sort(all(v));v.erase(unique(all(v)), v.end());};
+
+// Open in case of emergency X_X:
+// #define int      long long
+// #define double   long double
+
+int n, m;
+V<V<int>> box;
+V<int> fib;
+
+void preproc() {
+  fib.resize(14);
+  fib[1] = 1, fib[2] = 2;
+  for (int i = 3; i < fib.size(); ++i) {
+    fib[i] = fib[i-1] + fib[i-2];
+  }
+}
+
+bool ok(int w, int l, int h) {
+  // check if the w l h can fit the first two boxes
+  int f = fib[n], s = fib[n-1];
+  int lside = f + s;
+  // we need at least one side to fit lside, and the other two to fit f
+  V<int> sides{w, l, h};
+  sort(sides.begin(), sides.end());
+  return sides[2] >= lside && sides[1] >= f && sides[0] >= f;
+}
+
+void solve() {
+  read(n, m);
+  box.assign(m, V<int>(3));
+  for (int i = 0; i < m; ++i) {
+    read(box[i][0], box[i][1], box[i][2]);
+  }
+  string ans;
+  for (int i = 0; i < m; ++i) {
+    if (ok(box[i][0], box[i][1], box[i][2])) {
+      ans.push_back('1');
+    } else {
+      ans.push_back('0');
+    }
+  }
+  print(ans);
+}
+
+signed main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0), cout.tie(0);
+  preproc();
+  int tt = 1;
+  read(tt);
+  rep(tt) solve();
+}
