1ecc1c2124b97aa62504a6b9a78e624dc8be7b0d
Wanxuan Li <lzlu1018@gmail.com>
2026-01-10 08:44:11 -0500

    at25

---
+++ atcoder/abc440b.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(x)                          for (int _ = (0); _ < (x); ++_)
+template <typename T> using V           = vector<T>;
+template <typename T> using MinPQ       = priority_queue<T, V<T>, greater<T>>;
+template <typename T> using MaxPQ       = priority_queue<T, V<T>, less<T>>;
+
+template <typename T, typename = void> struct DefaultI : std::false_type {}; template <typename T> struct DefaultI<T, std::void_t<decltype(std::cin  >> std::declval<T&>())>> : std::true_type {};
+template <typename T, typename = void> struct DefaultO : std::false_type {}; template <typename T> struct DefaultO<T, std::void_t<decltype(std::cout << std::declval<T&>())>> : std::true_type {};
+template <typename T, typename = void> struct IsTuples : std::false_type {}; template <typename T> struct IsTuples<T, std::void_t<typename std::tuple_size<T>::type>>         : std::true_type {};
+template <typename T, typename = void> struct Iterable : std::false_type {}; template <typename T> struct Iterable<T, std::void_t<decltype(std::begin(std::declval<T>()))>>   : std::true_type {};
+template <typename T> char sp(T& t) {return(Iterable<T>::value||IsTuples<T>::value)?'\n':' ';};
+template <typename T> void re(T& t) {if constexpr(DefaultI<T>::value){cin>>t;}else if constexpr(Iterable<T>::value){for(auto&x:t)re(x);}else if constexpr(IsTuples<T>::value){std::apply([&](auto&...args){(re(args),...);},t);}};
+template <typename T> void pt(T& t) {if constexpr(DefaultO<T>::value){cout<<t;}else if constexpr(Iterable<T>::value){int i=0;for(auto&&x:t)((i++)?(cout<<sp(x),pt(x)):pt(x));}else if constexpr(IsTuples<T>::value){std::apply([&](auto const&...args){int i=0;(((i++)?(cout<<" ",pt(args)):pt(args)),...);},t);}};
+template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) {pt(t),((cout<<s,pt(ts)),...),cout<<'\n';}; void ps(const string&) {cout<<'\n';};
+template <typename... Ts> void print(const Ts& ...ts) {ps(" ", ts...);};
+template <typename... Ts> void read(Ts& ...ts) {((re(ts)), ...);};
+
+#ifndef ONLINE_JUDGE
+#define DEBUG(...) do { __VA_ARGS__ } while (0)
+#else
+#define DEBUG(...) do {} while (0)
+#endif
+
+int N;
+V<int> T;
+
+void solve() {
+  read(N);
+  T.resize(N), read(T);
+  V<pair<int, int>> A;
+  for (int i = 0; i < N; ++i) {
+    A.push_back({T[i], i + 1});
+  }
+  sort(A.begin(), A.end());
+  print(A[0].second, A[1].second, A[2].second);
+}
+
+signed main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0), cout.tie(0), cin.exceptions(cin.failbit);
+  int tt = 1; // read(tt);
+  rep(tt) solve();
+}
