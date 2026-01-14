05cd3e8b460c72db4dc7f07ff07c10fe4804e614
Wanxuan Li <lzlu1018@gmail.com>
2026-01-14 18:50:37 -0500

    at30

---
+++ atcoder/abc434_e.cpp
@@ -0,0 +1,75 @@
+// Problem: E - Distribute Bunnies
+// Contest: AtCoder - Ｓｋｙ Inc, Programming Contest 2025 (AtCoder Beginner Contest 434)
+// URL: https://atcoder.jp/contests/abc434/tasks/abc434_e
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(x)                          for (int _ = (0); _ < (x); ++_)
+template <typename T> using V           = vector<T>;
+template <typename T> using MinPQ       = priority_queue<T, V<T>, greater<T>>;
+template <typename T> using MaxPQ       = priority_queue<T, V<T>, less<T>>;
+
+template <typename T, typename = void> struct DefaultI : std::false_type {}; template <typename T> struct DefaultI<T, std::void_t<decltype(std::cin  >> std::declval<T&>())>> : std::true_type {};
+template <typename T, typename = void> struct DefaultO : std::false_type {}; template <typename T> struct DefaultO<T, std::void_t<decltype(std::cout << std::declval<T&>())>> : std::true_type {};
+template <typename T, typename = void> struct IsTuples : std::false_type {}; template <typename T> struct IsTuples<T, std::void_t<typename std::tuple_size<T>::type>>         : std::true_type {};
+template <typename T, typename = void> struct Iterable : std::false_type {}; template <typename T> struct Iterable<T, std::void_t<decltype(std::begin(std::declval<T>()))>>   : std::true_type {};
+template <typename T> char sp(T& t) {return(Iterable<T>::value||IsTuples<T>::value)?'\n':' ';};
+template <typename T> void re(T& t) {if constexpr(DefaultI<T>::value){cin>>t;}else if constexpr(Iterable<T>::value){for(auto&x:t)re(x);}else if constexpr(IsTuples<T>::value){std::apply([&](auto&...args){(re(args),...);},t);}};
+template <typename T> void pt(T& t) {if constexpr(DefaultO<T>::value){cout<<t;}else if constexpr(Iterable<T>::value){int i=0;for(auto&&x:t)((i++)?(cout<<sp(x),pt(x)):pt(x));}else if constexpr(IsTuples<T>::value){std::apply([&](auto const&...args){int i=0;(((i++)?(cout<<" ",pt(args)):pt(args)),...);},t);}};
+template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) {pt(t),((cout<<s,pt(ts)),...),cout<<'\n';}; void ps(const string&) {cout<<'\n';};
+template <typename... Ts> void print(const Ts& ...ts) {ps(" ", ts...);};
+template <typename... Ts> void read(Ts& ...ts) {((re(ts)), ...);};
+
+#ifndef ONLINE_JUDGE
+#define DEBUG(...) do { __VA_ARGS__ } while (0)
+#else
+#define DEBUG(...) do {} while (0)
+#endif
+
+#define int long long
+int N;
+V<pair<int, int>> rab;
+unordered_map<int, V<int>> grph;
+unordered_set<int> vis;
+
+void dfs(int u, int& V, int& E) {
+  vis.insert(u);
+  V++;
+  E += grph[u].size();
+  for (int v : grph[u]) {
+    if (!vis.count(v)) dfs(v, V, E);
+  }
+}
+
+void solve() {
+  read(N);
+  rab.resize(N), read(rab);
+  for (const auto [a, b] : rab) {
+    const int l = a - b, r = a + b;
+    grph[l].push_back(r), grph[r].push_back(l);
+  }
+  int ans = 0;
+  
+  for (const auto& [n, _] : grph) {
+    if (vis.count(n)) continue;
+    int V = 0, E = 0;
+    dfs(n, V, E);
+    E /= 2;
+    ans += (E == V - 1 ? V - 1 : V);
+  }
+  print(ans);
+}
+
+signed main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0), cout.tie(0), cin.exceptions(cin.failbit);
+  int tt = 1; // read(tt);
+  rep(tt) solve();
+}
