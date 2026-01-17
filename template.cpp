93e65856d3250b9fe9f4eee82a9421b917b1df0e
Wanxuan Li <lzlu1018@gmail.com>
2026-01-17 09:33:49 -0500

    update

--- template.cpp
+++ template.cpp
@@ -4,25 +4,16 @@
 using namespace std;
 
 inline namespace FastIO {
-template <typename T, typename = void> struct DefaultI : std::false_type {}; template <typename T> struct DefaultI<T, std::void_t<decltype(std::cin  >> std::declval<T&>())>> : std::true_type {};
-template <typename T, typename = void> struct DefaultO : std::false_type {}; template <typename T> struct DefaultO<T, std::void_t<decltype(std::cout << std::declval<T&>())>> : std::true_type {};
-template <typename T, typename = void> struct IsTuples : std::false_type {}; template <typename T> struct IsTuples<T, std::void_t<typename std::tuple_size<T>::type>>         : std::true_type {};
-template <typename T, typename = void> struct Iterable : std::false_type {}; template <typename T> struct Iterable<T, std::void_t<decltype(std::begin(std::declval<T>()))>>   : std::true_type {};
+#define SFINAE(x, ...) template <class, class = void> struct x : std::false_type {}; \
+                       template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
+SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>())); SFINAE(DefaultO, decltype(std::cout << std::declval<T &>())); SFINAE(IsTuples, typename std::tuple_size<T>::type); SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
 template <typename T> char sp(T& t) { return (Iterable<T>::value || IsTuples<T>::value) ? '\n' : ' '; }
-template <typename T> void re(T& t) {
-  if constexpr      (DefaultI<T>::value) { cin >> t; }
-  else if constexpr (Iterable<T>::value) { for (auto& x : t) re(x); }
-  else if constexpr (IsTuples<T>::value) { std::apply([&](auto& ...args) { (re(args), ...); }, t); }
-}
-template <typename T> void pt(T& t) {
-  if constexpr      (DefaultO<T>::value) { cout << t; }
-  else if constexpr (Iterable<T>::value) { int i = 0; for (auto&& x : t) ((i++) ? (cout << sp(x), pt(x)) : pt(x)); }
-  else if constexpr (IsTuples<T>::value) { std::apply([&](auto const& ...args) { int i = 0; (((i++) ? (cout << " ", pt(args)) : pt(args)), ...); }, t); }
-}
-template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) { pt(t), ((cout << s, pt(ts)), ...), cout << '\n'; }
-/* It passed locally but TLEsagain */ void ps(const string&) { cout << '\n'; }
+template <typename T> void re(T& t) { if constexpr (DefaultI<T>::value) { cin >> t; } else if constexpr (Iterable<T>::value) { for (auto& x : t) re(x); } else if constexpr (IsTuples<T>::value) { std::apply([&](auto& ...args) { (re(args), ...); }, t); } }
+template <typename T> void pt(T& t) { if constexpr (DefaultO<T>::value) { cout << t; } else if constexpr (Iterable<T>::value) { int i = 0; for (auto&& x : t) ((i++) ? (cout << sp(x), pt(x)) : pt(x)); } else if constexpr (IsTuples<T>::value) { std::apply([&](auto const& ...args) { int i = 0; (((i++) ? (cout << " ", pt(args)) : pt(args)), ...); }, t); } }
+template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) { pt(t), ((cout << s, pt(ts)), ...), cout << '\n'; } void ps(const string&) { cout << '\n'; }
 template <typename... Ts> void print(const Ts& ...ts) { FastIO::ps(" ", ts...); }
-template <typename... Ts> void read (Ts& ...ts) { ((FastIO::re(ts)), ...); }
+template <typename... Ts> void read(Ts& ...ts) { ((FastIO::re(ts)), ...); }
+#undef SFINAE
 }  // namespace FastIO
 
 inline namespace Util {
@@ -33,9 +24,9 @@ template <typename T> using MxPQ = priority_queue<T, V<T>, less<T>>;
 }  // namespace Util
 
 #ifndef ONLINE_JUDGE
-#  define debug(...) do { __VA_ARGS__ } while (0)
+  #define debug(...) do { __VA_ARGS__ } while (0)
 #else
-#  define debug(...) do {} while (0)
+  #define debug(...) do {} while (0)
 #endif
 
 void solve() {
