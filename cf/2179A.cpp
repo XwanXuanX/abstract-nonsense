29b51bd80c9bfd85174010df884068035b9909dc
Wanxuan Li <lzlu1018@gmail.com>
2026-01-19 21:00:42 -0500

    cf11

---
+++ cf/2179A.cpp
@@ -0,0 +1,48 @@
+// Problem: A. Blackslex and Password
+// Contest: Codeforces - Codeforces Round 1071 (Div. 3)
+// URL: https://codeforces.com/contest/2179/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+inline namespace FastIO {
+#define SFINAE(x, ...) template <class, class = void> struct x : std::false_type {}; template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
+SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>())); SFINAE(DefaultO, decltype(std::cout << std::declval<T &>())); SFINAE(IsTuples, typename std::tuple_size<T>::type); SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
+template <typename T> char sp(T& t) { return (Iterable<T>::value || IsTuples<T>::value) ? '\n' : ' '; }
+template <typename T> void re(T& t) { if constexpr (DefaultI<T>::value) { cin >> t; } else if constexpr (Iterable<T>::value) { for (auto& x : t) re(x); } else if constexpr (IsTuples<T>::value) { std::apply([&](auto& ...args) { (re(args), ...); }, t); } }
+template <typename T> void pt(T& t) { if constexpr (DefaultO<T>::value) { cout << t; } else if constexpr (Iterable<T>::value) { int i = 0; for (auto&& x : t) ((i++) ? (cout << sp(x), pt(x)) : pt(x)); } else if constexpr (IsTuples<T>::value) { std::apply([&](auto const& ...args) { int i = 0; (((i++) ? (cout << " ", pt(args)) : pt(args)), ...); }, t); } }
+template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) { pt(t), ((cout << s, pt(ts)), ...), cout << '\n'; } void ps(const string&) { cout << '\n'; }
+template <typename... Ts> void print(const Ts& ...ts) { FastIO::ps(" ", ts...); }
+template <typename... Ts> void read(Ts& ...ts) { ((FastIO::re(ts)), ...); }
+#undef SFINAE
+}  // namespace FastIO
+
+inline namespace Util {
+#define rep(x)                     for (int _ = (0); _ < (x); ++_)
+template <typename T> using V    = vector<T>;
+template <typename T> using MnPQ = priority_queue<T, V<T>, greater<T>>;
+template <typename T> using MxPQ = priority_queue<T, V<T>, less<T>>;
+}  // namespace Util
+
+#ifndef ONLINE_JUDGE
+  #define debug(...) do { __VA_ARGS__ } while (0)
+#else
+  #define debug(...) do {} while (0)
+#endif
+
+void solve() {
+  int k, x; read(k, x);
+  print(k * x + 1);
+}
+
+signed main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0), cout.tie(0), cin.exceptions(cin.failbit);
+  int tt = 1; read(tt);
+  rep(tt) solve();
+}
---
+++ cf/2179D.cpp
@@ -0,0 +1,70 @@
+// Problem: D. Blackslex and Penguin Civilization
+// Contest: Codeforces - Codeforces Round 1071 (Div. 3)
+// URL: https://codeforces.com/contest/2179/problem/D
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+inline namespace FastIO {
+#define SFINAE(x, ...) template <class, class = void> struct x : std::false_type {}; template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
+SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>())); SFINAE(DefaultO, decltype(std::cout << std::declval<T &>())); SFINAE(IsTuples, typename std::tuple_size<T>::type); SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
+template <typename T> char sp(T& t) { return (Iterable<T>::value || IsTuples<T>::value) ? '\n' : ' '; }
+template <typename T> void re(T& t) { if constexpr (DefaultI<T>::value) { cin >> t; } else if constexpr (Iterable<T>::value) { for (auto& x : t) re(x); } else if constexpr (IsTuples<T>::value) { std::apply([&](auto& ...args) { (re(args), ...); }, t); } }
+template <typename T> void pt(T& t) { if constexpr (DefaultO<T>::value) { cout << t; } else if constexpr (Iterable<T>::value) { int i = 0; for (auto&& x : t) ((i++) ? (cout << sp(x), pt(x)) : pt(x)); } else if constexpr (IsTuples<T>::value) { std::apply([&](auto const& ...args) { int i = 0; (((i++) ? (cout << " ", pt(args)) : pt(args)), ...); }, t); } }
+template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) { pt(t), ((cout << s, pt(ts)), ...), cout << '\n'; } void ps(const string&) { cout << '\n'; }
+template <typename... Ts> void print(const Ts& ...ts) { FastIO::ps(" ", ts...); }
+template <typename... Ts> void read(Ts& ...ts) { ((FastIO::re(ts)), ...); }
+#undef SFINAE
+}  // namespace FastIO
+
+inline namespace Util {
+#define rep(x)                     for (int _ = (0); _ < (x); ++_)
+template <typename T> using V    = vector<T>;
+template <typename T> using MnPQ = priority_queue<T, V<T>, greater<T>>;
+template <typename T> using MxPQ = priority_queue<T, V<T>, less<T>>;
+}  // namespace Util
+
+#ifndef ONLINE_JUDGE
+  #define debug(...) do { __VA_ARGS__ } while (0)
+#else
+  #define debug(...) do {} while (0)
+#endif
+
+// 1111, 0111, 0011, 1011, 0001, 0101, 1001, 1101, 0000, ...
+// 
+void solve() {
+  int n; read(n);
+  unordered_set<int> ava;
+  for (int i = 0; i < (1<<n) - 1; ++i) ava.insert(i); 
+  V<int> ans = {(1<<n) - 1};
+  int cnt = n - 1;
+  while (cnt) {
+    int base = 0;
+    for (int i = 0; i < cnt; ++i) {
+      base |= (1<<i);
+    }
+    const int left = n - (cnt+1);
+    for (int i = 0; i < (1<<left); ++i) {
+      const int amend = (i << (cnt+1)) | base;
+      ans.push_back(amend);
+      ava.erase(amend);
+    }
+    cnt--;
+  }
+  V<int> rest(ava.begin(), ava.end());
+  sort(rest.begin(), rest.end());
+  for (const int x : rest) ans.push_back(x);
+  print(ans);
+}
+
+signed main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0), cout.tie(0), cin.exceptions(cin.failbit);
+  int tt = 1; read(tt);
+  rep(tt) solve();
+}
