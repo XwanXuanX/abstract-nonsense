24af0cfda663f6687680ec30ce274110cb77fd5f
Wanxuan Li <lzlu1018@gmail.com>
2026-01-23 21:01:27 -0500

    at41

---
+++ atcoder/abc190_f.cpp
@@ -0,0 +1,94 @@
+// Problem: F - Shift and Inversions
+// Contest: AtCoder - AtCoder Beginner Contest 190
+// URL: https://atcoder.jp/contests/abc190/tasks/abc190_f
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+inline namespace FastIO {
+#define SFINAE(x, ...) template <class, class = void> struct x : std::false_type {}; template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
+SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>())); SFINAE(DefaultO, decltype(std::cout << std::declval<T &>())); SFINAE(IsTuples, typename std::tuple_size<T>::type); SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
+template <typename T> char sp(T& t) { return (Iterable<T>::value || IsTuples<T>::value) ? '\n' : ' '; }
+template <typename T> void re(T& t) { if constexpr (DefaultI<T>::value) { cin >> t; } else if constexpr (Iterable<T>::value) { for (auto& x : t) re(x); } else if constexpr (IsTuples<T>::value) { std::apply([&](auto& ...args) { (re(args), ...); }, t); } }
+template <typename T> void pt(T& t) { if constexpr (DefaultO<T>::value) { cout << t; } else if constexpr (Iterable<T>::value) { int i = 0; for (auto&& x : t) ((i++) ? (cout << sp(x), pt(x)) : pt(x)); } else if constexpr (IsTuples<T>::value) { std::apply([&](auto const& ...args) { int i = 0; (((i++) ? (cout << " ", pt(args)) : pt(args)), ...); }, t); } }
+template <typename T, typename... Ts> void ps(const string& s, T& t, Ts& ...ts) { pt(t), ((cout << s, pt(ts)), ...), cout << '\n'; } void ps(const string&) { cout << '\n'; }
+template <typename... Ts> void print(const Ts& ...ts) { FastIO::ps(" ", ts...); }
+template <typename... Ts> void read(Ts& ...ts) { ((FastIO::re(ts)), ...); }
+#undef SFINAE
+}  // namespace FastIO
+
+inline namespace Util {
+#define rep(x)                     for (int _ = (0); _ < (x); ++_)
+template <typename T> using V    = vector<T>;
+template <typename T> using MnPQ = priority_queue<T, V<T>, greater<T>>;
+template <typename T> using MxPQ = priority_queue<T, V<T>, less<T>>;
+}  // namespace Util
+
+#ifndef ONLINE_JUDGE
+  #define debug(...) do { __VA_ARGS__ } while (0)
+#else
+  #define debug(...) do {} while (0)
+#endif
+
+#define int long long
+using ll = long long;
+struct Fenwick {
+  int n;
+  V<ll> bit;
+  Fenwick(int n) : n(n), bit(n+1, 0) {}
+  
+  void add(int i, ll v) {
+    for (; i <= n; i += i & -i) bit[i] += v;
+  }
+
+  ll sum(int i) {
+    ll s = 0;
+    for (; i > 0; i -= i & -i) s += bit[i];
+    return s;
+  }
+};
+
+/*
+from k to k+1, the inversion number from [1, N-1] does not change
+need to find # of a[i] (1<=i<=N-1) such that a[i] > a[0], and add to answer
+0 3 1 5 4 2 9 6 8 7
+3 1 5 4 2 9 6 8 7 0
+1 5 4 2 9 6 8 7 0 3
+*/
+ll inversion(const V<int>& a) {
+  int n = a.size();
+  Fenwick fw(n);
+  ll inv = 0;
+  for (int i = n-1; i >= 0; --i) {
+    int idx = a[i] + 1;
+    inv += fw.sum(idx - 1);
+    fw.add(idx, 1);
+  }
+  return inv;
+}
+
+void solve() {
+  int n; read(n);
+  V<int> a(n); read(a);
+  auto under = [&](const int p) -> int { return p; };
+  auto above = [&](const int p) -> int { return n - 1 - p; };
+  int inv = inversion(a);
+  print(inv);
+  for (int i = 0; i < n - 1; ++i) {
+    inv -= under(a[i]);
+    inv += above(a[i]);
+    print(inv);
+  }
+}
+
+signed main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0), cout.tie(0), cin.exceptions(cin.failbit);
+  int tt = 1; // read(tt);
+  rep(tt) solve();
+}
