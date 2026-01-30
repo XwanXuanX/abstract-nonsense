6cc8317942ec5fe7d92c59dfd331b2fb9be3d917
Wanxuan Li <lzlu1018@gmail.com>
2026-01-29 20:51:25 -0500

    UPD

--- template.cpp
+++ template.cpp
@@ -2,14 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int    long long
+#define int long long
 #define double long double
-constexpr int INF = 1ll << 60;
-constexpr int MOD = 1'000'000'007; // 998'244'353;
-template <typename T> using MaxPQ = priority_queue<T, vector<T>, less<T>>;
-template <typename T> using MinPQ = priority_queue<T, vector<T>, greater<T>>;
-template <typename T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return true; } return false; }
-template <typename T> bool chmax(T& a, const T& b) { if (b > a) { a = b; return true; } return false; }
+constexpr int INF = 1ll<<60;
+constexpr int MOD = 1000000007; // 998244353;
+template<typename T> using MaxPQ = priority_queue<T,vector<T>,less<T>>;
+template<typename T> using MinPQ = priority_queue<T,vector<T>,greater<T>>;
+template<typename T> bool chmin(T& a,const T& b) {if(b<a){a=b;return true;}return false;}
+template<typename T> bool chmax(T& a,const T& b) {if(b>a){a=b;return true;}return false;}
 
 void solve() {
     
@@ -17,7 +17,7 @@ void solve() {
 
 signed main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0), cin.exceptions(cin.failbit);
-    int T = 1; cin >> T;
-    while (T--) solve();
+    cin.tie(0),cout.tie(0),cin.exceptions(cin.failbit);
+    int T=1; cin>>T;
+    while(T--) solve();
 }
