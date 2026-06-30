81654ee61b5937fa047f184caa492c5c5affc18c
Wanxuan Li <t378li@uwaterloo.ca>
2026-06-30 00:18:32 -0400

    update template

--- template.cpp
+++ template.cpp
@@ -5,19 +5,19 @@ using namespace std;
 #define INF (1ll<<60)
 
 #ifndef ONLINE_JUDGE
-    #include "debug.hpp"
-    #define debug(...) do { __VA_ARGS__ } while (0)
+        #include "debug.hpp"
+        #define debug(...) do { __VA_ARGS__ } while (0)
 #else
-    #define debug(...) do {} while (0)
+        #define debug(...) do {} while (0)
 #endif
 
 void solve() {
-    
+        
 }
 
 signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0),cout.tie(0);
-    int T=1; cin>>T;
-    while(T--) solve();
+        ios_base::sync_with_stdio(0);
+        cin.tie(0),cout.tie(0);
+        int T=1; cin>>T;
+        while(T--) solve();
 }
